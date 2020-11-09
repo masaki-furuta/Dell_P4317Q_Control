02502e6d965154d9979ed5d2cfedad57b26bda75
Masaki Furuta <masaki.furuta@gmail.com>
2020-11-10 01:14:41 +0900

    Updated scripts

--- scripts/p4317q_4k.sh
+++ scripts/p4317q_4k.sh
@@ -1,5 +1,21 @@
-
 #!/bin/bash
+
+python2 -m pip show pyserial > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+    set -xv
+    curl -kL https://bootstrap.pypa.io/get-pip.py | python2
+    python2 -m pip uninstall serial
+    python2 -m pip uninstall serial --user
+    python2 -m pip uninstall pyserial
+    python2 -m pip uninstall pyserial --user
+    python2 -m pip install pyserial --user
+    groups | grep dialout
+    if [[ $? -ne 0 ]]; then
+        grep dialout /etc/group || getent group dialout >> /etc/group
+        usermod -a -G dialout $USER
+    fi
+    set +xv
+fi
 
 #set -xv
 
